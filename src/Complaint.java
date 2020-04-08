9ddb559e69bee136b754dc761fbcbdfe714eb830
eden <eden04060925@gmail.com>
2020-04-08 18:20:18 +0300

    Eden's fields and constructors VERSION1 : Compleint , TeamOwner,Team,PrivatePage, Notifications

--- src/Complaint.java
+++ src/Complaint.java
@@ -1,2 +1,37 @@
 public class Complaint {
+int id;
+Fan fan;
+MainSystem system;
+
+    public Complaint(Fan fan, MainSystem system) {
+        this.fan = fan;
+        this.system = system;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Fan getFan() {
+        return fan;
+    }
+
+    public void setFan(Fan fan) {
+        this.fan = fan;
+    }
+
+    public MainSystem getSystem() {
+        return system;
+    }
+
+    public void setSystem(MainSystem system) {
+        this.system = system;
+    }
+
+    /** should hold SystemManager? */
 }
+
--- src/Notification.java
+++ src/Notification.java
@@ -1,2 +1,75 @@
+import java.util.HashSet;
+
 public class Notification {
+
+    String text;
+    /**should hold many?*/
+    SystemManager systemManager ;
+
+    /**must hold all?**/
+    HashSet<TeamManager> teamManagers;
+    HashSet<TeamOwner> teamOwners;
+    HashSet<Referee> referees;
+    HashSet<Fan> fans;
+    HashSet<Rfa> rfas;
+
+    public Notification(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public SystemManager getSystemManager() {
+        return systemManager;
+    }
+
+    public void setSystemManager(SystemManager systemManager) {
+        this.systemManager = systemManager;
+    }
+
+    public HashSet<TeamManager> getTeamManagers() {
+        return teamManagers;
+    }
+
+    public void setTeamManagers(HashSet<TeamManager> teamManagers) {
+        this.teamManagers = teamManagers;
+    }
+
+    public HashSet<TeamOwner> getTeamOwners() {
+        return teamOwners;
+    }
+
+    public void setTeamOwners(HashSet<TeamOwner> teamOwners) {
+        this.teamOwners = teamOwners;
+    }
+
+    public HashSet<Referee> getReferees() {
+        return referees;
+    }
+
+    public void setReferees(HashSet<Referee> referees) {
+        this.referees = referees;
+    }
+
+    public HashSet<Fan> getFans() {
+        return fans;
+    }
+
+    public void setFans(HashSet<Fan> fans) {
+        this.fans = fans;
+    }
+
+    public HashSet<Rfa> getRfas() {
+        return rfas;
+    }
+
+    public void setRfas(HashSet<Rfa> rfas) {
+        this.rfas = rfas;
+    }
 }

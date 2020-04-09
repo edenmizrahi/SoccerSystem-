1316d3cd6c2b91ffcf3fbc51245950dd90cb267a
yarden <yardensch95@gmail.com>
2020-04-09 18:57:07 +0300

    classes - match, teamManager, field, event and his extends

--- src/Injury.java
+++ src/Injury.java
@@ -1,2 +1,11 @@
+import javax.print.attribute.standard.DateTimeAtCreation;
+
 public class Injury extends Event {
+    /****Who keeps and maintains an events calender ?
+     * @param referee
+     * @param match
+     * @param dateTime****/
+    public Injury(Referee referee, Match match, DateTimeAtCreation dateTime) {
+        super(referee, match, dateTime);
+    }
 }
--- src/Offense.java
+++ src/Offense.java
@@ -1,2 +1,11 @@
+import javax.print.attribute.standard.DateTimeAtCreation;
+
 public class Offense extends Event {
+    /****Who keeps and maintains an events calender ?
+     * @param referee
+     * @param match
+     * @param dateTime****/
+    public Offense(Referee referee, Match match, DateTimeAtCreation dateTime) {
+        super(referee, match, dateTime);
+    }
 }

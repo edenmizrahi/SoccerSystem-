e3d1798e0822b91ddedd2e90c34ef99d01846b7b
or alfasi <oralfasi09@gmail.com>
2020-04-10 11:26:39 +0300

    test

---
+++ test/BudgetControlTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BudgetControlTest {
+
+    @Test
+    public void trytest() {
+        Assert.assertTrue(true);
+    }
+}

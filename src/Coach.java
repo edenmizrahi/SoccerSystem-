97cf2bd20d62fec3814ff12cdf3ddd51f7b2d2d0
yarden <yardensch95@gmail.com>
2020-04-12 10:48:28 +0300

    add attributes to player, coach, referee

--- src/Coach.java
+++ src/Coach.java
@@ -2,11 +2,13 @@ public class Coach extends Subscription implements PageOwner{
 
     Team coachTeam;
     PrivatePage coachPage;
+    private String roleAtTeam;
 
-    Coach(Subscription sub, MainSystem ms){
+    Coach(Subscription sub, MainSystem ms, String roleAtTeam){
         super(ms, sub.getName(), sub.getPhoneNumber(), sub.getEmail(), sub.getUserName(), sub.getPassword());
         //TODO add permissions
         //this.permissions.add();
+        this.roleAtTeam = roleAtTeam;
     }
 
     @Override

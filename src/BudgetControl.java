c6356c932302bfd37503fc31ecd87eb8873d1073
or alfasi <oralfasi09@gmail.com>
2020-04-16 23:43:28 +0300

    OR: main as Singleton add start System method and extenal System Stub

--- src/BudgetControl.java
+++ src/BudgetControl.java
@@ -2,26 +2,27 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BudgetControl {
     private static final Logger LOG = LogManager.getLogger();
     private long budget;
-    LinkedHashMap<String, Long> incomeAndExpenses;
+    private LinkedList<Report> incomeAndExpenses;
 
     BudgetControl(){
-        incomeAndExpenses = new LinkedHashMap<>();
+        incomeAndExpenses = new LinkedList<>();
         budget = 0;
     }
 
     public void addIncome(String typeOfIncome, long amount){
-        incomeAndExpenses.put(typeOfIncome, amount);
+        incomeAndExpenses.add(new Report(typeOfIncome,amount));
         budget += amount;
     }
     public void addExpense(String typeOfExpense, long amount) throws Exception{
         if (budget - amount >= 0) {
             amount = amount - amount * 2;
-            incomeAndExpenses.put(typeOfExpense, amount);
+            incomeAndExpenses.add(new Report(typeOfExpense, amount));
             budget = budget - amount;
         }
         else{
---
+++ src/Report.java
@@ -0,0 +1,40 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Report {
+    private String reason;
+    private long amount;
+    private Date now;
+    private DateFormat dateFormat= new SimpleDateFormat("dd/mm/yyyy");
+
+    public Report(String reason, long amount) {
+        this.reason = reason;
+        this.amount = amount;
+        this.now = new Date();
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public Date getNow() {
+        return now;
+    }
+
+    public void setNow(Date now) {
+        this.now = now;
+    }
+}

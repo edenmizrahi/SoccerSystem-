79af94b74c5ac638078b8cac4c198ae50f049579
eden <eden04060925@gmail.com>
2020-04-22 12:12:56 +0300

    ----Service---- Eden add initSystemFunction with Users in SystemOperationsController
    ---Domain-----
    Notifications-Done

--- src/Service/SystemOperationsController.java
+++ src/Service/SystemOperationsController.java
@@ -1,13 +1,11 @@
 package Service;
 
+import Domain.Enums.TeamManagerPermissions;
 import Domain.LeagueManagment.League;
 import Domain.LeagueManagment.Season;
 import Domain.LeagueManagment.Team;
-import Domain.Main;
 import Domain.MainSystem;
-import Domain.Users.Player;
-import Domain.Users.Referee;
-import Domain.Users.TeamOwner;
+import Domain.Users.*;
 
 import java.text.ParseException;
 import java.util.HashSet;
@@ -95,5 +93,64 @@ public class SystemOperationsController {
          }
 
      }
+
+
+
+
+        public static void initSystemObjects() throws Exception {
+            MainSystem system=MainSystem.getInstance();
+            system.startSystem();
+            SystemManager marioSystemManager=system.getSystemManagers().get(0);//there is only one system manager now (the default)
+            Team t1=new Team();
+            Team t2=new Team();
+
+            /**add Ilan as Team Owner (founder) of t1 ***/
+            Fan f1=new Fan(system, "Ilan", "0549716910","yossi@gmail.com", "YossiHamelech5", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            TeamRole ilanTeamOwner=new TeamRole(f1);
+            ilanTeamOwner.becomeTeamOwner();
+            ilanTeamOwner.getTeamOwner().addNewTeam(t1);
+            t1.setFounder(ilanTeamOwner.getTeamOwner());
+            t1.addTeamOwner(ilanTeamOwner.getTeamOwner());
+            /*********************************************/
+
+            /**Ilan subscribe moshe to be team Manager with the all permissions**/
+            Fan f2=new Fan(system, "Moshe", "0549716910","yossi@gmail.com", "YossiHamelech4", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            HashSet<TeamManagerPermissions> permissions=new HashSet<>();
+            permissions.add(TeamManagerPermissions.addRemoveEditPlayer);
+            permissions.add(TeamManagerPermissions.addRemoveEditTeamOwner);
+            permissions.add(TeamManagerPermissions.addRemoveEditCoach);
+            permissions.add(TeamManagerPermissions.addRemoveEditField);
+            permissions.add(TeamManagerPermissions.addToBudgetControl);
+            ilanTeamOwner.getTeamOwner().subscribeTeamManager(f2,t1,permissions);
+            /******************************************************************/
+
+            /**Gabi to be a player at t1 */
+            Fan f3= new Fan(system, "Gabi", "0549716910","yossi@gmail.com", "YossiHamelech3", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            TeamRole gabiPlayer=new TeamRole(f3);
+            gabiPlayer.becomePlayer();
+            ilanTeamOwner.getTeamOwner().addPlayer(gabiPlayer,"FFF",t1);
+            /****************************/
+
+            /**Tami to be player without team**/
+            Fan f4= new Fan(system, "Tami", "0549716910","yossi@gmail.com", "YossiHamelech2", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            TeamRole tamiPlayer=new TeamRole(f4);
+            tamiPlayer.becomePlayer();
+            /**********************************/
+
+            /**Haim to be a coach at t1 */
+            Fan f5= new Fan(system, "Haim", "0549716910","yossi@gmail.com", "YossiHamelech1", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            TeamRole haimCoach=new TeamRole(f5);
+            gabiPlayer.becomeCoach();
+            t1.setCoach(haimCoach.getCoach());
+            /****************************/
+
+            /**Dana to be RFA*/
+            Fan f6= new Fan(system, "Dana", "0549716910","yossi@gmail.com", "DanaBandana", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Rfa danaRFA=new Rfa(f6,system);
+            /*****************/
+
+            System.out.println("lalala");
+
+        }
 
 }

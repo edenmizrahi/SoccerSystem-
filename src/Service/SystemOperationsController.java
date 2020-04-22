a9661be330cd15235cc4b60b4e67703044754010
adiavinun <49281852+adiavinun@users.noreply.github.com>
2020-04-22 17:20:24 +0300

    accept reject tests

--- src/Service/SystemOperationsController.java
+++ src/Service/SystemOperationsController.java
@@ -111,7 +111,7 @@ public class SystemOperationsController {
 
 
             /**add Ilan as Team Owner (founder) of t1 ***/
-            Fan f1=new Fan(system, "Ilan", "0549716910","yossi@gmail.com", "YossiHamelech5", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Fan f1=new Fan(system, "Ilan", "0549716910","yossi@gmail.com", "Ilan", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             TeamRole ilanTeamOwner=new TeamRole(f1);
             ilanTeamOwner.becomeTeamOwner();
             ilanTeamOwner.getTeamOwner().addNewTeam(t1);
@@ -121,7 +121,7 @@ public class SystemOperationsController {
 
 
             /**add Avi as Team Owner (founder) of t2 ***/
-            Fan f7=new Fan(system, "Avi", "0549716910","yossi@gmail.com", "frkjamelech5", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Fan f7=new Fan(system, "Avi", "0549716910","yossi@gmail.com", "Avi", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             TeamRole aviTeamOwner=new TeamRole(f7);
             aviTeamOwner.becomeTeamOwner();
             aviTeamOwner.getTeamOwner().addNewTeam(t2);
@@ -131,14 +131,14 @@ public class SystemOperationsController {
 
 
             /**Ilan subscribe moshe to be team Manager with the all permissions**/
-            Fan f2=new Fan(system, "Moshe", "0549716910","yossi@gmail.com", "YossiHamelech4", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Fan f2=new Fan(system, "Moshe", "0549716910","yossi@gmail.com", "Moshe", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             HashSet<TeamManagerPermissions> permissions=new HashSet<>();
             permissions.add(TeamManagerPermissions.addRemoveEditPlayer);
             permissions.add(TeamManagerPermissions.addRemoveEditTeamOwner);
             permissions.add(TeamManagerPermissions.addRemoveEditCoach);
             permissions.add(TeamManagerPermissions.addRemoveEditField);
             permissions.add(TeamManagerPermissions.addToBudgetControl);
-            ilanTeamOwner.getTeamOwner().subscribeTeamManager(f2,t1,permissions);
+            TeamRole mosheTeamManager = ilanTeamOwner.getTeamOwner().subscribeTeamManager(f2,t1,permissions);
 
             /**add 11 players to t1*/
             add11PlayersToTeam(t1,ilanTeamOwner.getTeamOwner(),"d");
@@ -150,13 +150,13 @@ public class SystemOperationsController {
             /**********************/
 
             /**Tami to be player without team**/
-            Fan f4= new Fan(system, "Tami", "0549716910","yossi@gmail.com", "YossiHamelech2", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Fan f4= new Fan(system, "Tami", "0549716910","yossi@gmail.com", "Tami", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             TeamRole tamiPlayer=new TeamRole(f4);
             tamiPlayer.becomePlayer();
             /**********************************/
 
             /**Haim to be a coach at t1 */
-            Fan f5= new Fan(system, "Haim", "0549716910","yossi@gmail.com", "YossiHamelech1", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Fan f5= new Fan(system, "Haim", "0549716910","yossi@gmail.com", "Haim", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             TeamRole haimCoach=new TeamRole(f5);
             haimCoach.becomeCoach();
             haimCoach.getCoach().setCoachTeam(t1);
@@ -164,7 +164,7 @@ public class SystemOperationsController {
             /****************************/
 
             /**ben to be a coach at t2 */
-            Fan f8= new Fan(system, "ben", "0549716910","yossi@gmail.com", "ben", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            Fan f8= new Fan(system, "ben", "0549716910","yossi@gmail.com", "Ben", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             TeamRole benCoach=new TeamRole(f8);
             benCoach.becomeCoach();
             benCoach.getCoach().setCoachTeam(t2);
@@ -175,6 +175,10 @@ public class SystemOperationsController {
             Fan f6= new Fan(system, "Dana", "0549716910","yossi@gmail.com", "DanaBandana", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
             Rfa danaRFA=new Rfa(f6,system);
             /*****************/
+
+            /**Tamar to be just a Fan*/
+            Fan f9= new Fan(system, "Tamar", "0549716910","yossi@gmail.com", "Tamar", "Yossi123" ,MainSystem.birthDateFormat.parse("02-11-1996"));
+            /*********************/
 
             System.out.println("lalala");
 
@@ -206,11 +210,12 @@ public class SystemOperationsController {
      * @return list of match users.
      * @codeBy Eden
      */
-    public LinkedList<User> getUserByName(String name, SystemManager user) {
-        List<User> users = user.getSystem().getUsers();
+    public LinkedList<User> getUserByName(String name) {
+        List<User> users = MainSystem.getInstance().getUsers();
         LinkedList<User> matches = new LinkedList<User>();
         for (User cur : users) {
-            if (user.getName().equals(name)) ;
+            Fan curr = (Fan)cur;
+            if (curr.getName().equals(name)) ;
             matches.add(cur);
         }
         return matches;
@@ -221,15 +226,15 @@ public class SystemOperationsController {
      *User name is an unique field so this function return one user if there is user with
      *
      * @param userName
-     * @param user
      * @return
      * @codeBy Eden
      */
-    public User getUserByUserName(String userName, SystemManager user) {
-        List<User> users = user.getSystem().getUsers();
+    public User getUserByUserName(String userName) {
+        List<User> users = MainSystem.getInstance().getUsers();
         LinkedList<User> matches = new LinkedList<User>();
         for (User cur : users) {
-            if (user.getUserName().equals(userName)) {
+            Fan curr = (Fan)cur;
+            if (curr.getUserName().equals(userName)) {
                 return cur;
             }
         }

965f92850cc1fad163b1968950900d2793e0c5c9
eden <eden04060925@gmail.com>
2020-05-05 11:23:55 +0300

    UI

--- src/Presentation/LoginController.java
+++ src/Presentation/LoginController.java
@@ -1,4 +1,20 @@
 package Presentation;
 
-public class LoginController {
+import Domain.Users.User;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.event.ActionEvent;
+
+public class LoginController{
+@FXML
+    public void closeHandling(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
+
+
+    @FXML
+    public void validationHandling(MouseEvent mouseEvent) {
+
+    }
 }

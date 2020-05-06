8141bbd5d3a3d6c0d07fac7bd1c0783576ffdcf1
avital <avitalz57@gmail.com>
2020-05-06 14:34:25 +0300

    add gui2

--- src/Presentation/HomePageController.java
+++ src/Presentation/HomePageController.java
@@ -1,6 +1,38 @@
 package Presentation;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class HomePageController {
+
+
+    @FXML
+    Button viewSettings;
+    @FXML
+    Button registrationForGamesAlerts;
+    @FXML
+    Button myAlerts;
+
+    @FXML
+    public void signInMouseClickHandling(MouseEvent mouseEvent) throws IOException {
+
+        Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("FanDetails.fxml"));
+        Scene scene = new Scene(root, 700, 400);
+        //scene.getStylesheets().add(getClass().getResource("SignUp.css").toExternalForm());
+        stageTheEventSourceNodeBelongs.setScene(scene);
+
+    }
+
+
 
 
 

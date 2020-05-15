c9735b2165f3f451bfe8aa51443ad273713b41d1
yarden <yardensch95@gmail.com>
2020-05-15 17:54:09 +0300

    in season - boolean to policies

--- src/Domain/Users/Rfa.java
+++ src/Domain/Users/Rfa.java
@@ -377,6 +377,7 @@ public class Rfa extends Fan implements NotificationsUser {
             throw new NullPointerException();
         }
         season.getSchedulingPolicy().assign(season.getTeamsInCurrentSeasonLeagues(), season);
+        season.setPlaySchedualingPolicy(true);
         LOG.info(String.format("%s - %s", this.getUserName(), "start scheduling policy by the rfa"));
     }
 
@@ -403,6 +404,7 @@ public class Rfa extends Fan implements NotificationsUser {
 
         if(seasonExist){
             season.getCalculationPolicy().calculate(season.getTeamsInCurrentSeasonLeagues());
+            season.setPlayCalculationPolicy(true);
             LOG.info(String.format("%s - %s", this.getUserName(), "start calculating policy by the rfa "));
         }
         else{

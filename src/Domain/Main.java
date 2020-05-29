87d2c8a32979a1d9b084509535d0a14835c4de1a
eden <eden04060925@gmail.com>
2020-05-29 16:22:51 +0300

    ענעבי

--- src/Domain/Main.java
+++ src/Domain/Main.java
@@ -162,7 +162,7 @@ public class Main {
         FanController fc=new FanController();
         SystemOperationsController sop =new SystemOperationsController();
         sop.initSystemFromDB();
-        fc.getFanNotifications("shimi");
+//        System.out.println(uc.login("ilan","e111111"));
 //        SystemOperationsApplication soa=new SystemOperationsApplication();
 //        System.out.println(soa.signUp("TeamOwner","eden","0526621646","eden04060925@gmail.com","ilan","e111111","04-04-1994","true"));
 //        System.out.println(soa.signUp("Player","player","0526621646","eden04060925@gmail.com","player46","e111111","04-04-1994","true"));
@@ -178,11 +178,14 @@ public class Main {
 //        System.out.println(soa.signUp("Coach","eden","0526621646","eden04060925@gmail.com","coach12","e111111","04-04-1994","true"));
 
 //        String players="player46;player47;player48;player49;player50;player51;player52;player53;player54;player55;player56";
-//        UserController uc=new UserController();
-//        System.out.println(uc.login("ilan","e111111"));
-//        TeamManagementApplication te=new TeamManagementApplication();
+        UserController uc=new UserController();
+        System.out.println(uc.login("ilan","e111111"));
+//        fc.addMatchToFanMatchesFollow("ilan",)
+        //        TeamManagementApplication te=new TeamManagementApplication();
 //        te.makeTeamActive("ilan","amazingTeam",players,"coach12","bashField1");
 //        /**********/
+
+
 
 
 
